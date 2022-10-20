626abbf5afc2757bb7db8aacf31d6d15606f72ad
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-10-20 14:09:42 +0300

    Implement repository logic for refreshtoken

---
+++ Server/DAL/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Data;
+using DAL.Entities;
+using DAL.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class RefreshTokenRepository : Repository<RefreshToken>, IRefreshTokenRepository
+    {
+        public RefreshTokenRepository(GameStoreDbContext db) : base(db) {}
+    }
+}
