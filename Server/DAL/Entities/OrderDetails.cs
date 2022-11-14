e39c2e9f9e5dd7967b6163d67fce5413c75b94fe
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-11-14 15:52:12 +0200

    Add email to OrderDetails and add migration

--- Server/DAL/Entities/OrderDetails.cs
+++ Server/DAL/Entities/OrderDetails.cs
@@ -5,6 +5,7 @@ public class OrderDetails : BaseEntity
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string Email { get; set; }
     public string UserPhone { get; set; }
     public PaymentType PaymentType { get; set; }
     public DateTime OrderDate { get; set; } 
--- Server/DAL/Migrations/20221114132205_AddEmailToOrderDetails.Designer.cs
+++ Server/DAL/Migrations/20221114132205_AddEmailToOrderDetails.Designer.cs
@@ -4,6 +4,7 @@ using DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(GameStoreDbContext))]
-    partial class GameStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221114132205_AddEmailToOrderDetails")]
+    partial class AddEmailToOrderDetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
