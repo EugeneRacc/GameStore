b9b9af38853ca1bce9ae17c5d1c9f4e66c81d8e1
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-12-12 14:33:40 +0200

    New functions for auth service to update tokens with refresh, get user details

--- Client/game-store-frontend/src/app/services/authentication.service.ts
+++ Client/game-store-frontend/src/app/services/authentication.service.ts
@@ -1,16 +1,21 @@
 import {EventEmitter, Injectable} from '@angular/core';
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
 import {IRegistrationModel} from "../models/registration.model";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {IErrorModel} from "../models/error.model";
 import {ILoginModel} from "../models/login.model";
 import {ITokenModel} from "../models/token.model";
+import {IUserModel} from "../models/user.model";
+import {IRefreshTokenModel} from "../models/refresh.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  baseURL = 'https://localhost:7043/api/';
   loginOpened = new EventEmitter<boolean>();
+  isAuth = new BehaviorSubject<boolean>(false);
+  currentUser = new Subject<IUserModel | null>();
 
   constructor(private http: HttpClient) { this.loginOpened.emit(false); }
 
@@ -21,4 +26,18 @@ export class AuthenticationService {
   login(loginModel: ILoginModel): Observable<ITokenModel> {
     return this.http.post<ITokenModel>(`https://localhost:7043/api/authentication/login`, loginModel);
   }
+
+  getUserDetails(token:string): Observable<IUserModel> {
+    let tokenHeader = new HttpHeaders({'Authorization':'Bearer ' + token})
+    return this.http.get<IUserModel>(this.baseURL + "user", {headers: tokenHeader});
+  }
+
+  authorizeByRefresh(tokens: IRefreshTokenModel) : Observable<ITokenModel>{
+    return this.http.post<ITokenModel>(this.baseURL + "authentication/refresh", tokens);
+  }
+
+  setNewCurrentUser(user: IUserModel | null) {
+    this.currentUser.next(user);
+  }
+
 }
