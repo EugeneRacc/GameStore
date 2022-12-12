e2a067bdb579259315c463188fc05f05e1675369
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-12-12 14:38:41 +0200

    Check if user is authenticated when site is downloading and delete tokens when site closed

--- Client/game-store-frontend/src/app/app.component.ts
+++ Client/game-store-frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {AuthenticationService} from "./services/authentication.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'game-store-frontend';
+export class AppComponent implements OnInit{
+  @HostListener('window:unload', ['$event']) beforeUnloadHandler() {
+    if (!localStorage.getItem("refresh")) {
+      localStorage.removeItem('token');
+    }
+  }
+
+  constructor(private authService: AuthenticationService) {
+  }
+  ngOnInit(): void {
+    this.checkIfUserAuthorized();
+  }
+
+  checkIfUserAuthorized() {
+    let refresh = localStorage.getItem('refresh');
+    let token = localStorage.getItem('token');
+    if (refresh && token){
+      this.authService.authorizeByRefresh({ token: token, refreshToken: refresh })
+        .subscribe(token => {
+          localStorage.setItem('token', token.token);
+          this.authService.isAuth.next(true);
+          this.authService.getUserDetails(token.token)
+            .subscribe(user => this.authService.setNewCurrentUser(user))
+        })
+    }
+  }
 }
