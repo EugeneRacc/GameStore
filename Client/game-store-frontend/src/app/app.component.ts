423feabe7555ad3db91c8bd033e1d5c7a44d5f19
EugeneRacc <forjbracc@gmail.com>
2022-12-23 09:47:34 +0200

    Fix authentication bugs when user roles disappears and update app.component.ts to get tokens by TokenService

--- Client/game-store-frontend/src/app/app.component.ts
+++ Client/game-store-frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {AuthenticationService} from "./services/authentication.service";
+import {Router} from "@angular/router";
+import {TokenService} from "./services/token.service";
 
 @Component({
   selector: 'app-root',
@@ -13,22 +15,27 @@ export class AppComponent implements OnInit{
     }
   }
 
-  constructor(private authService: AuthenticationService) {
+  constructor(private authService: AuthenticationService, private router: Router,
+              private tokenService: TokenService) {
   }
   ngOnInit(): void {
     this.checkIfUserAuthorized();
   }
 
   checkIfUserAuthorized() {
-    let refresh = localStorage.getItem('refresh');
-    let token = localStorage.getItem('token');
+    let refresh = this.tokenService.getRefreshToken();
+    let token = this.tokenService.getToken();
     if (refresh && token){
       this.authService.authorizeByRefresh({ token: token, refreshToken: refresh })
         .subscribe(token => {
-          localStorage.setItem('token', token.token);
+          this.tokenService.saveToken(token.token);
           this.authService.isAuth.next(true);
           this.authService.getUserDetails(token.token)
             .subscribe(user => this.authService.setNewCurrentUser(user))
+        }, error => {
+          console.log(error);
+          this.tokenService.signOut();
+          this.router.navigateByUrl('/main-page/login');
         })
     }
   }
