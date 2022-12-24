6ca4f9efdd8b6b823b8522f9d9d8eaad714d5bd1
Evgenii Ivanchenko <forjbracc@gmail.com>
2022-12-25 01:10:35 +0200

    Fix logic of functions

--- Client/game-store-frontend/src/app/services/cart.service.ts
+++ Client/game-store-frontend/src/app/services/cart.service.ts
@@ -1,28 +1,41 @@
 import { Injectable } from '@angular/core';
 import {IGame} from "../models/game.model";
 import {IOrderModel} from "../models/order.model";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   gamesInOrder: IOrderModel[] = [];
-  constructor() { }
+  gamesInOrderS = new BehaviorSubject<IOrderModel[]>([]);
+  constructor() {
+    this.gamesInOrderS
+      .subscribe(order => this.gamesInOrder = order);
+  }
 
   addGameToOrder(game: IGame) {
     let indexOfGame = this.gamesInOrder.findIndex(x => x.game.id === game.id);
     if (indexOfGame > -1) {
       this.gamesInOrder[indexOfGame].amount += 1;
+      this.gamesInOrderS.next(this.gamesInOrder);
     }
     else {
-      this.gamesInOrder.push({game: game, amount: 0} as IOrderModel)
+      this.gamesInOrder.push({game: game, amount: 1} as IOrderModel)
+      this.gamesInOrderS.next(this.gamesInOrder);
     }
+    console.log(this.gamesInOrder);
   }
 
   setLowerAmountOfGame(game: IGame) {
     let indexOfGame = this.gamesInOrder.findIndex(x => x.game.id === game.id);
     if (indexOfGame > -1) {
       this.gamesInOrder[indexOfGame].amount -= 1;
+      if (this.gamesInOrder[indexOfGame].amount <= 0) {
+        this.gamesInOrder.splice(indexOfGame, 1);
+        this.gamesInOrderS.next(this.gamesInOrder);
+      }
+      this.gamesInOrderS.next(this.gamesInOrder);
     }
     else {
       console.error('Id not found');
@@ -33,6 +46,7 @@ export class CartService {
     let indexOfGame = this.gamesInOrder.findIndex(x => x.game.id === game.id);
     if (indexOfGame > -1) {
       this.gamesInOrder.splice(indexOfGame, 1);
+      this.gamesInOrderS.next(this.gamesInOrder);
     }
     else {
       console.error('Id not found');
